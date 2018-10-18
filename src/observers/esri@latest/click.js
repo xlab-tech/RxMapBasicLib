aeb7bc00b6bfd080b8dd64a732dbd98bea46cd83
victor <vrubinat@gmail.com>
2018-10-18 08:43:23 +0200

    fix esri click observer

--- src/observers/esri@latest/click.js
+++ src/observers/esri@latest/click.js
@@ -1,5 +1,6 @@
 import { fromEventPattern } from 'rxjs/internal/observable/fromEventPattern';
 import { map } from 'rxjs/internal/operators/map';
+import { filter } from 'rxjs/internal/operators/filter';
 import { mergeMap } from 'rxjs/internal/operators/mergeMap';
 
 const click = context => () => {
@@ -13,17 +14,29 @@ const click = context => () => {
     addClickHandler,
     removeClickHandler,
   );
-
   const mapFunction = event => ({ lat: event.mapPoint.latitude, lng: event.mapPoint.longitude });
 
-  if (name === 'marker') {
+  if (name === 'marker' || name === 'addData') {
     const mergeMapFunction = event => esri.view.hitTest(event).then((response) => {
-      const graphic = response.results.filter(obj => obj === value);
-      return graphic;
+      let filterFunction = obj => obj.graphic === value;
+      if (Array.isArray(value)) {
+        filterFunction = obj => value.includes(obj.graphic);
+      }
+      const graphic = response.results.filter(filterFunction)
+        .map(el => el.graphic)
+        .map(el => el.attributes)
+        .shift();
+
+      if (graphic && Object.keys(graphic).length > 0) {
+        event.stopPropagation();
+        return graphic;
+      }
+      return null;
     });
 
     return $strem.pipe(
       mergeMap(mergeMapFunction),
+      filter(el => el !== null),
     );
   }
 
